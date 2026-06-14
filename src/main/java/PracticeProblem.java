4fc19c20c1ef55f0cf82f94e6648bc4545eca2c0
SalmaDanane <343249595@tdsb.ca>
2026-06-14 18:08:53 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class PracticeProblem {
@@ -6,4 +7,81 @@ public class PracticeProblem {
 
 	}
 
+	public static int intChecker() {
+		
+		Scanner input = new Scanner(System.in);
+		int num;
+		
+		System.out.print("Input an integer: ");
+		
+		while (!(input.hasNextInt())) {
+			input.nextLine();
+			System.out.print("Input an integer: ");
+			}
+		
+		num = input.nextInt();
+		input.nextLine();
+		return num;
+		
+	}
+
+	public static double doubleChecker() {
+		
+		Scanner input = new Scanner(System.in);
+		double num;
+		System.out.print("Input a number: ");
+		
+		while (!(input.hasNextDouble())) {
+			
+			input.nextLine();
+			System.out.print("Input a number: ");
+			
+			}
+		
+		num = input.nextDouble();
+		input.nextLine();
+		return num;
+		
+	}
+
+	public static boolean booleanChecker() {
+		
+		Scanner input = new Scanner(System.in);
+		boolean value;
+		
+		System.out.print("Input a boolean: ");
+		
+		while (!(input.hasNextBoolean())) {
+			
+			input.nextLine();
+			System.out.print("Input a boolean: ");
+			}
+		
+		value = input.nextBoolean();
+		input.nextLine();
+		return value;
+		
+	}
+
+	public static boolean multipleIntChecker() {
+		
+		Scanner input = new Scanner(System.in);
+		
+		for (int i = 0; i < 4; i++) {
+			System.out.print("Input an integer: ");
+			
+		 while (!(input.hasNextInt())) {
+			input.nextLine();
+			System.out.print("Input an integer: ");
+			 
+			}
+			
+			input.nextInt();
+		}
+		
+		return true;
+	}
+
 }
+
+
